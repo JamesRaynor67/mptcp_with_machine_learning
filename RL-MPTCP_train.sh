c418cf3dff3201a935b29e9d5960d6cc30c41c33
Hong <hongjiaminghjm@gmail.com>
2017-06-20 20:01:30 +0900

    Rename script

--- RL-MPTCP_train.sh
+++ RL-MPTCP_train.sh
@@ -3,15 +3,11 @@
 export DISPLAY=:0  # # This is only for remote execution. However, above is recommended on the Internet (but not works perfect
 declare -A RLConfig; declare -A Ns3Config
 
-tcp_buffer="262144"    
-router_b_buffer="1"
-router_c_buffer="1"
-link_b_BER="0"
 # tcpBuffer="$1"    
 # routerBBuffer="$2"
 # routerCBuffer="$3"
 # link_b_BER="$4"
-topology_id="0"
+# topology_id="0"
 scheduler="RL-Choose"
 dirPath=""
 
@@ -20,11 +16,13 @@ function preProcess(){
   dirPath="/home/hong/result_figure/0_static_${timestamp}"
   cp "/home/hong/result_figure/template.csv" "/home/hong/result_figure/statistic.csv"
   mkdir $dirPath
-  cp "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/${BASH_SOURCE[0]}" "$dirPath/script.sh" 
+  cp "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/${BASH_SOURCE[0]}" "$dirPath/script_train.sh"
+  cp "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/RL-MPTCP_experiment_parameters.sh" "$dirPath/script_parameters.sh"
+  source ./RL-MPTCP_experiment_parameters.sh
 }
 
 function postProcess(){
-  local newName="${dirPath}/statistic_${Ns3Config["tcpBuffer"]}_${Ns3Config["linkBBuffer"]}_${Ns3Config["linkCBuffer"]}_${Ns3Config["link_b_BER"]}_${Ns3Config["topology_id"]}.csv"
+  local newName="${dirPath}/statistic_${Ns3Config["tcp_buffer"]}_${Ns3Config["router_b_buffer"]}_${Ns3Config["router_c_buffer"]}_${Ns3Config["link_b_BER"]}_${Ns3Config["topology_id"]}.csv"
   mv "/home/hong/result_figure/statistic.csv" ${newName}
 }
 
@@ -45,27 +43,7 @@ function record(){
 }
 
 function loadRLPara(){
-  RLConfig+=(["forceReply"]=$scheduler ["maxEpisode"]=20 ["scheduler"]=$scheduler ["sendInterval"]="100000" ["savePath"]="${dirPath}")
-}
-
-function loadParamExp21() {
-  Ns3Config+=(["link_a_BW"]="400Kbps" ["link_b_BW"]="100Kbps" ["link_c_BW"]="100Kbps" ["link_a_delay"]="6ms" ["link_b_delay"]="250ms" ["link_c_delay"]="15ms" \
-              ["tcp_buffer"]="$tcp_buffer" ["router_b_buffer"]="$router_b_buffer" ["router_c_buffer"]="$router_c_buffer" ["link_b_BER"]="$link_b_BER" ["topology_id"]="$topology_id" ["experiment"]="Exp21")
-}
-
-function loadParamExp22() {
-  Ns3Config+=(["link_a_BW"]="400Kbps" ["link_b_BW"]="100Kbps" ["link_c_BW"]="100Kbps" ["link_a_delay"]="6ms" ["link_b_delay"]="15ms" ["link_c_delay"]="15ms" \
-              ["tcp_buffer"]="$tcp_buffer" ["router_b_buffer"]="$router_b_buffer" ["router_c_buffer"]="$router_c_buffer" ["link_b_BER"]="$link_b_BER" ["topology_id"]="$topology_id" ["experiment"]="Exp22")
-}
-
-function loadParamExp23() {
-  Ns3Config+=(["link_a_BW"]="400Kbps" ["link_b_BW"]="200Kbps" ["link_c_BW"]="100Kbps" ["link_a_delay"]="6ms" ["link_b_delay"]="15ms" ["link_c_delay"]="15ms" \
-              ["tcp_buffer"]="$tcp_buffer" ["router_b_buffer"]="$router_b_buffer" ["router_c_buffer"]="$router_c_buffer" ["link_b_BER"]="$link_b_BER" ["topology_id"]="$topology_id" ["experiment"]="Exp23")
-}
-
-function loadParamExp24() {
-  Ns3Config+=(["link_a_BW"]="400Kbps" ["link_b_BW"]="200Kbps" ["link_c_BW"]="50Kbps" ["link_a_delay"]="6ms" ["link_b_delay"]="15ms" ["link_c_delay"]="15ms" \
-              ["tcp_buffer"]="$tcp_buffer" ["router_b_buffer"]="$router_b_buffer" ["router_c_buffer"]="$router_c_buffer" ["link_b_BER"]="$link_b_BER" ["topology_id"]="$topology_id" ["experiment"]="Exp24")
+  RLConfig+=(["forceReply"]=$scheduler ["maxEpisode"]=100 ["scheduler"]=$scheduler ["sendInterval"]="100000" ["savePath"]="${dirPath}")
 }
 
 #####################
@@ -77,15 +55,29 @@ for (( episodeNum=0; episodeNum<${RLConfig["maxEpisode"]}; episodeNum++ ))
 do
   unset Ns3Config; declare -A Ns3Config
   
-  case "$(( ( RANDOM % 2 ) ))" in
-  1) loadParamExp21
+  case "$(( ( RANDOM % 10 ) ))" in
+  1) loadDefaultBufferSetting; loadParamExp21
     ;;
-  *) loadParamExp24
+  2) loadDefaultBufferSetting; loadParamExp22
+    ;;    
+  3) loadDefaultBufferSetting; loadParamExp23
+    ;;
+  4) loadDefaultBufferSetting; loadParamExp24
+    ;;
+  5) loadDefaultBufferSetting; loadParamExp25
+    ;;
+  6) loadDefaultBufferSetting; loadParamExp26
+    ;;
+  7) loadDefaultBufferSetting; loadParamExp27
+    ;;
+  8) loadDefaultBufferSetting; loadParamExp28
+    ;;
+  9) loadDefaultBufferSetting; loadParamExp29
+    ;;                        
+  *) loadDefaultBufferSetting; loadParamExp29 # I can't left it empty. Or episode count gets wrong.
     ;;
   esac
 
-  # loadParamExp15
-  
   runNS3 "$episodeNum"
   # record "$episodeNum"
  if !(($episodeNum % 20))
